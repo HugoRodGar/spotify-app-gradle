a11862259fc849ebe29636e1833fae00d73ad38b
HugoRodGar <conbigames@gmail.com>
2026-02-26 01:28:46 +0100

    add single memory instance to songs feature

---
+++ src/main/java/edu/iesam/features/songs/data/SongMemLocalDataSource.java
@@ -0,0 +1,37 @@
+package edu.iesam.features.songs.data;
+
+import edu.iesam.features.songs.domain.Song;
+
+import java.util.ArrayList;
+
+public class SongMemLocalDataSource {
+
+    private static SongMemLocalDataSource instance = null;
+
+    private ArrayList<Song> songsMemStorage = new ArrayList<>();
+
+    public SongMemLocalDataSource() {
+        initData();
+    }
+
+    public void initData() {
+        Song song1 = new Song("1", "MemSong1", "Lil Pepe", "2:30");
+        songsMemStorage.add(song1);
+
+        Song song2 = new Song("2", "MemSong2", "Pepe", "2:30");
+        songsMemStorage.add(song2);
+    }
+
+    public ArrayList<Song> findAll() {
+        return songsMemStorage;
+    }
+
+    public static SongMemLocalDataSource newInstance(){
+        if (instance == null){
+            instance = new SongMemLocalDataSource();
+        }
+
+        return instance;
+    }
+
+}
